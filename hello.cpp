d7a1ebab2875fe0b9f57e2bf5b9a116c5d71da5f
Gary Lopez <glopez@cwolf.cs.sonoma.edu>
2014-10-30 10:04:55 -0700

    Using ARGV

--- hello.cpp
+++ hello.cpp
@@ -1,1 +1,14 @@
-"Hello, World"
+// Project 4:Lab 05
+// Author: Gabriela Lopez
+#include <iostream>
+using namespace std;
+
+int main (int argc, char* argv[])
+{
+  if (argc < 3){
+      cout << argv[10] << endl;
+ 
+      return 1;
+ }
+}
+"Hello, #{ARGV.first}!"
